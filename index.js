bdcc2c7437508c6015c7535006cce6f069563979
James Leslie <jaleslie02@gmail.com>
2018-09-08 21:49:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan") {(
-  consol.log("The name is correct"))
+if (name === "Susan") {
+  consol.log("The name is correct")
   
 } else {
   consol.log("Expected name to be Susan")
